c765d93a34d50ff161dee3c9323daa0dc6a5b929
GEORGEMUTALE <mutalegeorge367@gmail.com>
2024-05-12 10:52:30 +0200

    prime factors

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
-pub fn factors(n: u64) -> Vec<u64> {
-    unimplemented!("This should calculate the prime factors of {n}")
+pub fn factors(mut n: u64) -> Vec<u64> {
+    let mut prime_factors = Vec::new();
+    let mut divisor = 2;
+
+    while n > 1 {
+        while n % divisor == 0 {
+            prime_factors.push(divisor);
+            n /= divisor;
+        }
+        divisor += 1;
+    }
+
+    prime_factors
 }
